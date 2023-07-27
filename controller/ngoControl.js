5b2e451ff26b3febee1007c33bd99356b47a998e
addission <108756447+aad999@users.noreply.github.com>
2023-07-28 01:48:10 +0530

    console.log

--- controller/ngoControl.js
+++ controller/ngoControl.js
@@ -98,6 +98,7 @@ exports.verifyNGO = async (req, res) => {
 
 exports.cancelVerification = async (req, res) => {
     const { id } = req.query;
+    console.log(id);
 
     try {
         // Find the NGO by ID
